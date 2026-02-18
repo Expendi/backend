20b7ba42ad473736e258f7b5f99828b3b05d4d23
porkytheblack <porkytheblack@gmail.com>
2026-02-18 14:41:48 +0300

    patch: expendi setup

---
+++ scratch-pad.ts
@@ -0,0 +1,10 @@
+// support for user wallets
+// support for a server controlled wallets - server wallet
+// support for agent controlled wallets - server wallet
+// contract connectors for different contract i.e include abis, addresses, etc
+// jobber - dealing with scheduled jobs
+// heartbeat for agentic checks.
+// adapters for getting information for the agent e.g coinmarketcap etc
+// user id and action and payload
+// record all transactions for an internal ledger 
+// transaction categories: - user defined categories and others
